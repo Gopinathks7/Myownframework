13f580dc6c9874ac47b1d7d17105b4131110b2ea
Gopinath.kumsi <gopinathks7@gmail.com>
2020-03-17 12:39:24 +0530

    name

--- src/org/javas/P1.java
+++ src/org/javas/P1.java
@@ -6,14 +6,30 @@ public class P1 {
 	{
 	int rem=0;
 	int last=0;
+	 int flag=0;
 	for(int i=10;i<=100;i++)
 	{
 		 int number=i;
-		 
+		
 		 rem=number%10;
+while(number>0)
+{
+	last=number%10;
+	number=number/10;
+	if(!(rem==last))
+	{
+		flag=0;
+		break;
+	}
 
+flag++;
+}
+if(flag>0)
+{
+	System.out.print(i+"    ");
+}
 	}
 			
 	}
-
 }
+
