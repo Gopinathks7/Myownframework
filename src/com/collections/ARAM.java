411a88c5f18c26d5075e8ef53322459909ceefe2
Gopinath.kumsi <gopinathks7@gmail.com>
2020-03-17 12:12:30 +0530

    kumsi

---
+++ src/com/collections/ARAM.java
@@ -0,0 +1,20 @@
+package com.collections;
+
+import java.util.Scanner;
+
+public class ARAM {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter an number");
+		Scanner scan = new Scanner(System.in);
+		int number=scan.nextInt();
+		int temp=number;
+		while(temp!=0)
+		{
+			
+		}
+
+	}
+
+}
---
+++ src/org/javas/P1.java
@@ -0,0 +1,19 @@
+package org.javas;
+
+public class P1 {
+
+	public static void main(String[] args) 
+	{
+	int rem=0;
+	int last=0;
+	for(int i=10;i<=100;i++)
+	{
+		 int number=i;
+		 
+		 rem=number%10;
+
+	}
+			
+	}
+
+}
