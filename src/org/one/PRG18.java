158eb279b4d0e251457e6a13e61b2f38a560e589
Gopinath.kumsi <gopinathks7@gmail.com>
2020-04-08 14:55:13 +0530

    gopi

---
+++ src/org/one/PRG18.java
@@ -0,0 +1,13 @@
+package org.one;
+
+import java.util.ArrayList;
+
+public class PRG18 {
+
+	public static void main(String[] args) {
+		String name="gopinath";
+		ArrayList<String>al=new ArrayList<String>();
+
+	}
+
+}
