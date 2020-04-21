9d38b7e359dd008f263dd4cbce36581b9f75b7e2
Gopinath.kumsi <gopinathks7@gmail.com>
2020-04-21 13:47:27 +0530

    some

--- src/org/one/PRG20.java
+++ src/org/one/PRG20.java
@@ -10,15 +10,18 @@ public class PRG20
 	   
 	public static  int countWeekendDays(int year, int month) 
 	{
-		int amount=0;
-		int weekendamount=0;
-		int dayofmonth=0;
-		int weekends=0;
+		
+		int weekendamount=5;
+		int amoutexcludingweekend=4;
+		int total=0;
+
+
 		Calendar calendar = Calendar.getInstance();
 	     calendar.set(year, month - 1, 1);
 	    int daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
 
 	    int count = 0;
+	    
 	    for (int day = 1; day <= daysInMonth; day++) 
 	    {
 	        calendar.set(year, month - 1, day);
@@ -30,15 +33,21 @@ public class PRG20
 	        }
 	    }
 	    int days=daysInMonth-count;
+	    int totalweekendamount=count*weekendamount;
+	    int sumtotal=amoutexcludingweekend *days;
+	    total=totalweekendamount+sumtotal;
 	    System.out.println("Number of saturday & Sunday is "+count);
 	    System.out.println("Number of days in the manth is "+daysInMonth);
-	    System.out.println("Total noumber of day exculiding weekends is "+days);
+	    System.out.println("Total number of day excluding weekends is "+days);
+	    System.out.println("Total weekend amount is "+totalweekendamount);
+	    System.out.println("Total amount of week days is "+sumtotal);
+	    System.out.println("The Total amount of the month is "+total);
 	    return count;
 	}
 	public static void main(String[] args) 
 	{
 	
-		countWeekendDays(2020, 1);
+		countWeekendDays(2020, 10);
 		
 	}
 
