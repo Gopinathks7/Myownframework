af131305ed214feea3e276d66342712931170e32
Gopinath kumsi <Gopinathks7@gmail.com>
2020-05-18 14:09:01 +0530

    g

---
+++ src/com/learningjava/Test2.java
@@ -0,0 +1,15 @@
+package com.learningjava;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+int i=10;
+do
+{
+	System.out.println("The value of i is "+i);
+	i--;
+}
+while(i<0);
+	}
+
+}
---
+++ src/com/learningjava/Test3.java
@@ -0,0 +1,16 @@
+package com.learningjava;
+
+import java.sql.NClob;
+import java.util.Calendar;
+
+public class Test3 {
+
+	public static void main(String[] args) 
+	{
+Calendar c= Calendar.getInstance();
+
+c.setFirstDayOfWeek(1);
+System.out.println(c);
+	}
+
+}
